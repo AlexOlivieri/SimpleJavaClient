5a7f1f21d6f18c9eaa41f25fd16f3f8ee7e75a78
AlexOlivieri <alex.olivieri81@gmail.com>
2017-02-20 11:35:36 +0100

    First Commit For Java Client

---
+++ SimpleTestJava/src/main/java/lesson1/Contract.java
@@ -0,0 +1,10 @@
+package lesson1;
+
+public class Contract {
+	
+	public final static String SERVER_URL = "http://88.99.66.215:8080/";
+	public final static String SERVICE_ENTRY = "WebServiceIntroductionExample2/";
+	public final static String SUM = "sum";
+	public final static String SUM2 = "sum2/1,4";
+	
+}
---
+++ SimpleTestJava/src/main/java/lesson1/TestTheSum.java
@@ -0,0 +1,48 @@
+package lesson1;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class TestTheSum {
+	
+	public static void main(String[] args){
+		
+		try{
+			String stringUrl = Contract.SERVER_URL.concat(Contract.SERVICE_ENTRY).concat(Contract.SUM2);
+			URL url = new URL(stringUrl);
+
+			HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+			conn.setRequestMethod("GET");
+			conn.setRequestProperty("Accept", "application/json");
+			
+			if (conn.getResponseCode() != 200) {
+				throw new RuntimeException("Failed : HTTP error code : "
+						+ conn.getResponseCode());
+			}
+			BufferedReader br = new BufferedReader(new InputStreamReader(
+					(conn.getInputStream())));
+
+				String output;
+				System.out.println("Output from Server .... \n");
+				while ((output = br.readLine()) != null) {
+					System.out.println(output);
+				}
+
+				conn.disconnect();
+
+			  } catch (MalformedURLException e) {
+
+				e.printStackTrace();
+
+			  } catch (IOException e) {
+
+				e.printStackTrace();
+
+			  }
+	}
+
+}
